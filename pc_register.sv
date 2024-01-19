aade78d1261faeec0cfcedba537f8b8d45cd1d61
FaisalShahkar <146974792+FaisalShahkar@users.noreply.github.com>
2024-01-19 16:51:09 +0500

    Create pc_register.sv

---
+++ pc_register.sv
@@ -0,0 +1,22 @@
+module register(input wire clk,
+                input wire reset,
+                input wire enable,
+                input wire  [31:0] data_in,  
+                output wire [31:0] data_out);
+  reg [31:0] out;
+  always@(posedge clk or posedge reset)
+    begin
+      if(reset)
+        begin
+          out <= 32'b0;
+        end
+      else if(~enable)
+        begin
+          out <= out;
+        end
+      else
+        begin 
+          out <= data_in;
+        end
+    end
+endmodule
