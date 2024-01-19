4949489de1e0b4810c7c5df897124515a95642e5
FaisalShahkar <146974792+FaisalShahkar@users.noreply.github.com>
2024-01-19 16:52:49 +0500

    instr_mem.sv

---
+++ instr_mem.sv
@@ -0,0 +1,17 @@
+module instr_mem(input wire clk,
+                   input wire reset,
+                   input wire  [31:0] base_address,  
+                   output wire [31:0] data_out);
+  
+  reg [31:0] imem [79:0]; // 32*20 instruction memory size
+  reg [31:0] out;
+  $readmemh("instructions.txt",imem);
+  always@(posedge clk)
+    begin
+      out = {imem[base_address+3].imem[base_address+2].imem[base_address+1].imem[base_address]};
+    end
+  assign data_out = out;
+  
+  
+  
+endmodule
