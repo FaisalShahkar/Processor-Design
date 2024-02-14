06a66e961942a405859a2072a8de18364badac60
Faisal Shahkar <146974792+FaisalShahkar@users.noreply.github.com>
2024-02-14 21:50:02 +0500

    Update instr_mem.sv

--- instr_mem.sv
+++ instr_mem.sv
@@ -3,7 +3,7 @@ module instr_mem(input wire clk,
                    input wire  [31:0] base_address,  
                    output wire [31:0] data_out);
   
-  reg [31:0] imem [79:0]; // 32*20 instruction memory size
+  reg [7:0] imem [79:0]; // 32*20 instruction memory size
   reg [31:0] out;
   $readmemh("instructions.txt",imem);
   always@(posedge clk)
