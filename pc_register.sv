4104ae283714bc725bb7050f4a87e82d824e1c54
Faisal Shahkar <146974792+FaisalShahkar@users.noreply.github.com>
2024-02-14 20:55:43 +0500

    Update pc_register.sv

--- pc_register.sv
+++ pc_register.sv
@@ -1,22 +1,27 @@
-module register(input wire clk,
-                input wire reset,
-                input wire enable,
-                input wire  [31:0] data_in,  
-                output wire [31:0] data_out);
+module register(
+  input wire clk,
+  input wire reset,
+  input wire enable,
+  input wire [31:0] data_in,
+  output wire [31:0] data_out
+);
+
   reg [31:0] out;
-  always@(posedge clk or posedge reset)
-    begin
-      if(reset)
-        begin
-          out <= 32'b0;
-        end
-      else if(~enable)
-        begin
-          out <= out;
-        end
-      else
-        begin 
-          out <= data_in;
-        end
+
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      out <= 32'b0;
     end
-endmodule
+    else begin
+      if (~enable) begin
+        out <= out;
+      end
+      else begin
+        out <= data_in;
+      end
+    end
+  end
+
+  assign data_out = out;
+
+endmodule;
