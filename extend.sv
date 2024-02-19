3ec6964de134901e86735a73df8b9068f556e26e
Faisal Shahkar <146974792+FaisalShahkar@users.noreply.github.com>
2024-02-19 18:25:22 +0500

    Update extend.sv

--- extend.sv
+++ extend.sv
@@ -1,13 +1,13 @@
 module extend(
-    input wire [11:0] input_data,
-    output reg [31:0] output_data
+    input wire [11:0] imm,
+    output reg [31:0] ExtImm
 );
 
     always @* begin
-        if (input_data[11] == 1'b0) begin
-            output_data = {input_data, {20{1'b0}}}; // Fill remaining bits with 0s
+        if ( imm[11] == 1'b0) begin
+            ExtImm = { imm, {20{1'b0}}}; // Fill remaining bits with 0s
         end else begin
-            output_data = {input_data, {20{1'b1}}}; // Fill remaining bits with 1s
+            ExtImm = { imm, {20{1'b1}}}; // Fill remaining bits with 1s
         end
     end
 
